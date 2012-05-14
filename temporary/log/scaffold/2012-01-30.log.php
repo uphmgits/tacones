9a2012458b81a7239efc2442abefe8c1c91cb98a
Juan Arguello <arguelloj@gmail.com>
2012-05-14 08:05:36 -0700

    delete cache

--- temporary/log/scaffold/2012-01-30.log.php
+++
@@ -1,1 +0,0 @@
-2012-01-30 20:03:32 +00:00 --- Error: Missing image - /public/header-imgs/core_signin_header.png / /home/zapato/public_html/public/header-imgs/core_signin_header.png
--- temporary/log/scaffold/2012-03-22.log.php
+++
@@ -1,4 +0,0 @@
-2012-03-22 06:47:36 +00:00 --- Error: Missing image - /application/themes/musharraf/./images/arrow-selected.jpg / /home/zapato/public_html/application/themes/musharraf/./images/arrow-selected.jpg
-2012-03-22 07:17:07 +00:00 --- Error: Missing image - /application/themes/musharraf/./images/arrow-selected.jpg / /home/zapato/public_html/application/themes/musharraf/./images/arrow-selected.jpg
-2012-03-22 07:17:19 +00:00 --- Error: Missing image - /application/themes/musharraf/./images/arrow-selected.jpg / /home/zapato/public_html/application/themes/musharraf/./images/arrow-selected.jpg
-2012-03-22 07:17:38 +00:00 --- Error: Missing image - /application/themes/musharraf/./images/arrow-selected.jpg / /home/zapato/public_html/application/themes/musharraf/./images/arrow-selected.jpg
